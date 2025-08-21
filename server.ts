d6caf484f7f8e4b389e522875540dc513583a68e
mobasshirCode <mobasshir.code@gmail.com>
2025-08-22 02:50:10 +0530

    changes for vercel

--- server.ts
+++ server.ts
@@ -9,23 +9,37 @@ import invoiceRoutes from "./routes/invoiceRoutes";
 import productRoutes from "./routes/productRoutes";
 
 const app = express();
+
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend
-    credentials: true, // allow cookies/headers from ffrontend
+    origin: "https://invoice-frontend-taupe.vercel.app/",
+    credentials: true,
   })
 );
+
 app.use(express.json());
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products/invoice", invoiceRoutes);
 app.use("/api/products", productRoutes);
 
-mongoose.connect(process.env.MONGO_URI as string)  // .env me yaad se daalna h
-  .then(() => console.log("MongoDB Atlas Connected Hogya"))
-  .catch(err => console.log("MongoDB Error:", err));
-
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+
+let isConnected = false;
+
+async function connectDB() {
+  if (isConnected) return;
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string);
+    isConnected = true;
+    console.log("MongoDB Atlas Connected");
+  } catch (err) {
+    console.error("MongoDB Error:", err);
+  }
+}
+connectDB();
+
+module.exports = app;
